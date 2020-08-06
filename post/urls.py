26ee3d44bad18194f086eece79095a2b447c612d
2JooYeon <lewndus7749@naver.com>
2020-08-06 13:53:59 +0900

    commit for seunAh

---
+++ post/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('list/', views.read_blog_list, name = 'read_blog_list'),
+    path('detail/<int:blog_id>', views.read_blog_detail, name = 'read_blog_detail'),
+    path('new/', views.blog_new, name = 'blog_new'),
+    path('edit/<int:blog_id>', views.update_blog, name= 'update_blog'),
+    path('create/', views.create_blog, name="create_blog"),
+]
