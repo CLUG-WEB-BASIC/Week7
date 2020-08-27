c152ad43e00299c9adaaf5d1ce9f13ec6efc1bae
SA_21 <safg0421@cau.ac.kr>
2020-08-27 14:55:47 +0900

    week7

--- accounts/views.py
+++ accounts/views.py
@@ -9,8 +9,29 @@ def signup(request):
                 username=request.POST['username'],
                 password=request.POST['password1']
             )
+            login(request,user)
             return redirect('read_blog_list')
         else:
             return render(request, 'accounts/signup.html')
     else:
         return render(request, 'accounts/signup.html')
+
+def signin(request):
+    #authenticate
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username =username, password=password)
+        #login 성공
+        if user is not None:
+            login(request, user)
+            return redirect('read_blog_list')
+        #다시 login화면으로
+        else :
+            return render(request, 'accounts/signin.html', {'error':'incorrect ID or password'})
+    else:
+        return render(request, 'accounts/signin.html')
+
+def signout(request):
+    logout(request)
+    return redirect('read_blog_list')
